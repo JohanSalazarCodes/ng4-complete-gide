a86b430e3304f1cb814462867f90495571ae9e23
Johan Salazar <johan.itcr@gmail.com>
2022-12-21 12:32:55 -0600

    End of section 6

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,12 +6,22 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent { 
+export class RecipeListComponent {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simple a test', 
+    new Recipe('A Test Recipe', 'This is simple a test',
+    'https://images.immediate.co.uk/production/volatile/sites/30/2022/05/Airfryer-hero-image-2a442a6.jpg?quality=90&resize=504%2C458'),
+
+    new Recipe('Another Recipe', 'Another simple a test',
     'https://images.immediate.co.uk/production/volatile/sites/30/2022/05/Airfryer-hero-image-2a442a6.jpg?quality=90&resize=504%2C458')
   ];
-  
+
+
+  onRecipeSelected(recipe: Recipe) {
+
+    this.recipeWasSelected.emit(recipe);
+
+  }
 
 }
