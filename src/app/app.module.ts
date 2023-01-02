82732045caca7dfbd890e26069e36f38e7876181
Johan Salazar <johan.itcr@gmail.com>
2023-01-02 12:36:54 -0600

    End of Section 12: Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.co
 import { FormsModule } from '@angular/forms';
 import { DrodownDirective } from './shared/drodown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -24,10 +27,13 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     ShoppingListComponent,
     ShoppingEditComponent,
     DrodownDirective,
+    RecipeStartComponent,
+    RecipeEditComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
