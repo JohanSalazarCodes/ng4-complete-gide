b84e85bb7a81c02b84252b558717d6203c2609cc
Johan Salazar <johan.itcr@gmail.com>
2023-01-10 22:54:47 -0600

    End of Section 20: Authentication & Route Protection

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
@@ -6,12 +8,30 @@ import { DataStorageService } from '../shared/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor(private dataStorageService: DataStorageService) {}
+  private userSub: Subscription;
+  isAuthenticated = false;
+
+  constructor(private dataStorageService: DataStorageService,
+              private authService: AuthService) {}
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+    });
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
+  }
+
+  onLogout(){
+    this.authService.logout();
   }
 
   onFetchData() {
